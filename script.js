cbb29646a1d17d0963c76908645921ca44d7b30b
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2020-10-26 11:44:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Bonnie L"
+let userName = "bonniel1507";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
